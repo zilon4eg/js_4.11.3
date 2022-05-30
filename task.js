903901276c576c072d6a289cb612c64f6b4c049d
Igor <hdd500t@yandex.ru>
2022-05-30 21:55:05 +0300

    work on mistakes

--- task.js
+++ task.js
@@ -1,33 +1,29 @@
 document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let downloadFile = new XMLHttpRequest();
+    let uploadFile = new XMLHttpRequest();
     const progress = document.getElementById('progress');
 
-    downloadFile.onload = () => {
-        if (downloadFile.status != 200) { // HTTP ошибка?
+    uploadFile.onload = () => {
+        if (uploadFile.status != 200) { // HTTP ошибка?
           // обработаем ошибку
-          alert('Ошибка: ' + downloadFile.status);
+          alert('Ошибка: ' + uploadFile.status);
           return;
         }
-    }    
+    }
 
-    downloadFile.onprogress = (e) => {
+    uploadFile.upload.onprogress = (e) => {
         if (e.lengthComputable) {
             let percentComplete = e.loaded / e.total;
             progress.value = percentComplete;
         }
     }
 
-    // так как "e.total" на 17 строке равно нулю, а "e.lengthComputable = false"
-    // заполняем прогрессбар на 100% по факту завершения загрузки
-    downloadFile.onload = () => {
-        if (downloadFile.readyState === downloadFile.DONE) {
-            progress.value = 1.0;
-        }
+    uploadFile.upload.onload = () => {
+        progress.value = 1.0;
     }
 
-    downloadFile.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    var formData = new FormData(document.getElementById('form'));
-    downloadFile.send(formData);
+    uploadFile.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    const formData = new FormData(document.getElementById('form'));
+    uploadFile.send(formData);
 });
